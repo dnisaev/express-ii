8933c8dcb8527c2430a63d1f1ce193cb4088c42e
dnisaev <dnisaev@yandex.ru>
2024-03-29 18:47:59 +0300

    add product parameter

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    let helloMessage = "1";
-    res.send(helloMessage);
+const products = [{ title: 'tomato' }, { title: "orange" }];
+const addresses = [{ value: 'Moscow' }, { value: "Minsk" }];
+app.get('/products', (req, res) => {
+    res.send(products);
+});
+app.get('/products/:productTitle', (req, res) => {
+    let product = products.find(p => p.title === req.params.productTitle);
+    if (product) {
+        res.send(product);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import express, {Request, Response} from 'express'
 const app = express()
 const port = 3000
+const products = [{title: 'tomato'}, {title: "orange"}]
+const addresses = [{value: 'Moscow'}, {value: "Minsk"}]
 
-app.get('/', (req: Request, res: Response) => {
-  let helloMessage= "1";
-  res.send(helloMessage)
+app.get('/products', (req: Request, res: Response) => {
+  res.send(products)
+})
+
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+  let product = products.find(p => p.title === req.params.productTitle)
+
+  if(product) {
+    res.send(product)
+  } else {
+    res.send(404)
+  }
+
+
 })
 
 app.listen(port, () => {
